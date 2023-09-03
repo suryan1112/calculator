49949d4cbec3a8674edd639c60ccb16ad69b7990
suryan gupta <139356206+suryan1112@users.noreply.github.com>
2023-09-03 14:00:37 +0530

    Update contact.js
    audio is fixed

--- contact.js
+++ contact.js
@@ -9,11 +9,15 @@ var shift=false;
 var alpha=false;
 var mute=false;
 
-document.getElementById("number_button").volume=.2;
+const audio1=document.getElementById("number_button")
+const audio2=document.getElementById("small_button")
+audio1.volume=.4;
 
 document.getElementById('7').addEventListener('click', function() {
     if(on) screen.innerText+="7";
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -21,6 +25,8 @@ document.getElementById('7').addEventListener('click', function() {
 document.getElementById('8').addEventListener('click', function() {
     if(on) screen.textContent+="8";
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -28,6 +34,8 @@ document.getElementById('8').addEventListener('click', function() {
 document.getElementById('9').addEventListener('click', function() {
    if(on) screen.textContent+="9";
 
+   audio1.pause();
+   audio1.currentTime=0
    if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -36,6 +44,8 @@ document.getElementById('del').addEventListener('click', function() {
     if(on){var str=screen.innerText;
     screen.innerHTML=str.replace(str[str.length-1],"");}
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -57,6 +67,8 @@ document.getElementById('ac').addEventListener('click', function() {
     saver2=screen2.textContent;
     screen2.textContent="0.";}
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -64,6 +76,8 @@ document.getElementById('ac').addEventListener('click', function() {
 document.getElementById('4').addEventListener('click', function() {
     if(on)screen.textContent+="4"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -71,6 +85,8 @@ document.getElementById('4').addEventListener('click', function() {
 document.getElementById('5').addEventListener('click', function() {
     if(on)screen.textContent+="5"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -78,6 +94,8 @@ document.getElementById('5').addEventListener('click', function() {
 document.getElementById('6').addEventListener('click', function() {
     if(on)screen.textContent+="6"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -85,6 +103,8 @@ document.getElementById('6').addEventListener('click', function() {
 document.getElementById('*').addEventListener('click', function() {
     if(on)screen.textContent+="*"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -92,6 +112,8 @@ document.getElementById('*').addEventListener('click', function() {
 document.getElementById('/').addEventListener('click', function() {
     if(on)screen.textContent+="/"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -99,6 +121,8 @@ document.getElementById('/').addEventListener('click', function() {
 document.getElementById('1').addEventListener('click', function() {
     if(on)screen.textContent+="1"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -106,6 +130,8 @@ document.getElementById('1').addEventListener('click', function() {
 document.getElementById('2').addEventListener('click', function() {
     if(on) screen.textContent+="2"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -113,6 +139,8 @@ document.getElementById('2').addEventListener('click', function() {
 document.getElementById('3').addEventListener('click', function() {
     if(on)screen.textContent+="3"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -120,6 +148,8 @@ document.getElementById('3').addEventListener('click', function() {
 document.getElementById('+').addEventListener('click', function() {
     if(on)screen.textContent+="+"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -127,6 +157,8 @@ document.getElementById('+').addEventListener('click', function() {
 document.getElementById('-').addEventListener('click', function() {
     if(on)screen.textContent+="-"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -134,6 +166,8 @@ document.getElementById('-').addEventListener('click', function() {
 document.getElementById('0').addEventListener('click', function() {
     if(on)screen.textContent+="0"
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -141,6 +175,8 @@ document.getElementById('0').addEventListener('click', function() {
 document.getElementById('.').addEventListener('click', function() {
     if(on)screen.textContent+="."
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -152,6 +188,8 @@ document.getElementById('10x').addEventListener('click', function() {
         }
         screen.textContent+="*10**"
     }
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -173,11 +211,15 @@ document.getElementById('ans').addEventListener('click', function() {
         screen.textContent+=saver2;
     else screen.textContent+=screen2.textContent;}
 
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 });
 document.getElementById('=').addEventListener('click', function() {
+    audio1.pause();
+    audio1.currentTime=0
     if(!mute) document.getElementById("number_button").play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
@@ -191,8 +233,9 @@ document.querySelector(".complex>div>div:nth-child(2)").addEventListener('click'
     if(on){
         if(shift){math("Math.cbrt(");shiftdone();return; }
          math("Math.sqrt(")
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -200,8 +243,9 @@ document.querySelector(".complex>div>div:nth-child(3)").addEventListener('click'
     if(on && screen.textContent.length){
         if(shift) { screen.textContent+="**3"; shiftdone(); return;}
         if(screen.textContent.length) screen.textContent+="**2";
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -212,8 +256,9 @@ document.querySelector(".complex>div>div:nth-child(4)").addEventListener('click'
             shiftdone();
             return;
         }else screen.textContent+="**";
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -221,8 +266,9 @@ document.querySelector(".complex>div>div:nth-child(5)").addEventListener('click'
     if(on){
         if(shift){math("10**");shiftdone();return; }
         math("Math.log10(")
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -231,8 +277,9 @@ document.querySelector(".complex>div>div:nth-child(6)").addEventListener('click'
         if(shift){math("2.72**");shiftdone();return; }
         math("Math.log(")
         // this.style.border=;
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -241,8 +288,9 @@ document.querySelector(".complex>div>div:nth-child(7)").addEventListener('click'
     var str=screen.innerText;
     if(str[0]!="-") screen.innerHTML="-"+screen.innerHTML;
     else {screen.innerText=screen.innerText.substring(1);}}
-
-    if(!mute) document.getElementById("small_button").play()
+audio2.pause();
+audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -250,8 +298,9 @@ document.querySelector(".complex>div>div:nth-child(10)").addEventListener('click
     if(on){
         if(shift){math("Math.asin(");shiftdone();return; }
         math("Math.sin(")
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -259,8 +308,9 @@ document.querySelector(".complex>div>div:nth-child(11)").addEventListener('click
     if(on){
         if(shift){math("Math.acos(");shiftdone();return; }
         math("Math.cos(")
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -268,21 +318,24 @@ document.querySelector(".complex>div>div:nth-child(12)").addEventListener('click
     if(on){
         if(shift){math("Math.atan(");shiftdone();return; }
         math("Math.tan(")
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
 document.querySelector(".complex>div>div:nth-child(15)").addEventListener('click',function(){
-    if(on)screen.textContent+="("
-    if(!mute) document.getElementById("small_button").play()
+    if(on)scaudio2.pause();
+    audio2.currentTime=0;reen.textContent+="("
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
 document.querySelector(".complex>div>div:nth-child(16)").addEventListener('click',function(){
     if(on) screen.textContent+=")"
-
-    if(!mute) document.getElementById("small_button").play()
+audio2.pause();
+audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -293,8 +346,9 @@ document.getElementById('onoff').addEventListener('click', function() {
         document.querySelector("#screen #shift").style.opacity=.3;
         screen.textContent="";
         screen2.textContent="0.";
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 });
@@ -302,8 +356,9 @@ document.querySelector(".complex>#shift").addEventListener('click', function() {
     if(on){
         shift=true;
         document.querySelector("#screen #shift").style.opacity=1;
-    } 
-    if(!mute) document.getElementById("small_button").play()
+    } audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);   
 });
@@ -317,8 +372,9 @@ document.querySelector(".complex>#alpha").addEventListener('click', function() {
             alpha=false;
             document.querySelector("#screen #alpha").style.opacity=.3;
         }
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);    
 });
@@ -326,24 +382,27 @@ document.querySelector(".complex>div>div:nth-child(17)").addEventListener('click
     if(on){
         if(shift){screen.textContent+=";"; shiftdone(); return;}
         screen.textContent+=","
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
 document.querySelector(".complex>div>div:nth-child(8)").addEventListener('click',function(){
     if(on){
         if(shift){screen.textContent+="<"; shiftdone(); return;}  
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
 document.querySelector(".complex>div>div:nth-child(14)").addEventListener('click',function(){
     if(on){
         if(shift){screen.textContent+=">"; shiftdone(); return;}
-    }
-    if(!mute) document.getElementById("small_button").play()
+    }audio2.pause();
+    audio2.currentTime=0;
+    if(!mute) audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
@@ -386,8 +445,9 @@ document.addEventListener("keydown",(event)=>{
      screen2.textContent="0.";}
 })
 document.getElementById("mode").addEventListener("click",()=>{
-    mute=!mute;
-    document.getElementById("small_button").play()
+  audio2.pause();
+  audio2.currentTime=0;  mute=!mute;
+    audio2.play()
     this.style.boxShadow="none"
     setTimeout(()=>{this.style.boxShadow="2px 2px 4px .5px";},200);
 })
